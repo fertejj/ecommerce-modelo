b0b18359b0453c9f2ec2b9223e7c13c9015d994b
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-05-07 01:39:37 -0300

    Start app develop

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'signin', component: SigninComponent},
+    { path: 'signup', component: SignupComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", ".pages/**/*.{html,js}", ".pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '',
+        secondary: '',
+        bg: '',
+      }
+    },
+  },
+  plugins: [],
+};
