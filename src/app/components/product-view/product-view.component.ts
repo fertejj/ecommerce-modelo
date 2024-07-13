1796463e6164ebda0bb114f4d9b5fae55798c966
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-07-13 00:18:49 -0300

    componente ya trae data del service

--- src/app/components/product-view/product-view.component.ts
+++ src/app/components/product-view/product-view.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Producto } from '../../models/producto.model';
+import { Subscription } from 'rxjs';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-product-view',
@@ -7,6 +10,34 @@ import { Component } from '@angular/core';
   templateUrl: './product-view.component.html',
   styleUrl: './product-view.component.css'
 })
-export class ProductViewComponent {
+export class ProductViewComponent implements OnInit, OnDestroy {
+
+  producto: Producto[] = [];
+  productoSub: Subscription | undefined;
+
+  constructor(private productoService: ProductoService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.productoSub = this.productoService.getProducto()
+    .subscribe({
+      next: (productos: Producto[]) => {
+        this.producto = productos
+        console.log(this.producto)
+      },
+      error: (err: any) => {
+        console.error('GET request error:', err);
+      },
+      complete: () => {
+        console.log('GET request completed');
+      },
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    this.productoSub?.unsubscribe()
+  }
 
 }
