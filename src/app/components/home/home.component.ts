c5adb12dccbb19d327849ae22d98862e402f6191
Fernando Javier Tejerina <101528363+fertejj@users.noreply.github.com>
2024-07-24 12:16:00 -0300

    modificaciones de style en modal y home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { ProductListComponent } from '../product-list/product-list.component';
 import { CartModalComponent } from "../cart-modal/cart-modal.component";
+import { HeroSliderComponent } from "../hero-slider/hero-slider.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductListComponent, CartModalComponent],
+  imports: [ProductListComponent, CartModalComponent, HeroSliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
